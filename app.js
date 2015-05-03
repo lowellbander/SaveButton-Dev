9fd632c11d91b877f9158b1c05c12bbc1d33f20e
Lowell Bander <lowellbander@gmail.com>
2015-05-03 11:51:30 -0700

    hits spotify search endpoint

--- app.js
+++ app.js
@@ -9,11 +9,25 @@ app.get('/', function (req, res) {
 
 URL = 'http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=lowellbander&api_key=410af592466bfb635d96c11f77053117&format=json'
 
+searchURL = 'https://api.spotify.com/v1/search?type=track&q='
+
 request(URL, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-          //console.log(body) 
-          foo = JSON.parse(body)['recenttracks']['track'][0];
-          console.log(foo);
+    
+      currentTrack = JSON.parse(body)['recenttracks']['track'][0];
+      console.log(currentTrack);
+     
+      searchURL += encodeURIComponent(
+                      currentTrack['artist']['#text'] + ' ' +
+                      currentTrack['name'] + ' ' +
+                      currentTrack['album']['#text']);
+
+      console.log(searchURL);
+
+      request(searchURL, function (error, response, body) {
+        console.log(body);
+      });
+
     } else {
       console.log("ERROR");
     }
