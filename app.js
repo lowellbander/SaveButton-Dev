2b0042fb9fb89a25df800e53d29bb8fc25f15779
Lowell Bander <lowellbander@gmail.com>
2015-05-03 11:38:21 -0700

    does lastfm request

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var app = express();
+var request = require('request');
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 
 });
 
-var server = app.listen(3000, function () {
+URL = 'http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=lowellbander&api_key=410af592466bfb635d96c11f77053117&format=json'
 
-  var host = server.address().address;
-  var port = server.address().port;
+request(URL, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+          //console.log(body) 
+          foo = JSON.parse(body)['recenttracks']['track'][0];
+          console.log(foo);
+    } else {
+      console.log("ERROR");
+    }
+})
 
-  console.log('Example app listening at http://%s:%s', host, port);
-
-});
